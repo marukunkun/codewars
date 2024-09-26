4678ae44ed55b2e8aac98627fb35111daab28b59
marukun <marukun.dev@gmail.com>
2024-09-26 19:30:45 +0300

    new codewars challenge

---
+++ 8kyu/opposite_number.js
@@ -0,0 +1,16 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+// Solution 1
+function opposite(number) {
+    return number * -1
+}
+
+// Solution 2
+function opposite(number) {
+    return -number
+}
