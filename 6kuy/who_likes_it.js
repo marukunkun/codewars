5dbd6cd335ffd9d7913ae0b318ee7cb43a78b448
marukun <marukun.dev@gmail.com>
2024-10-05 07:59:17 +0300

    new codewars challenge

--- 6kuy/who_likes_it.js
+++ 6kuy/who_likes_it.js
@@ -32,4 +32,4 @@ console.log(likes([])) // 'no one likes this'
 console.log(likes(['Peter'])) // 'Peter likes this'
 console.log(likes(['Jacob', 'Alex'])) // 'Jacob and Alex like this'
 console.log(likes(['Max', 'John', 'Mark'])) // 'Max, John and Mark like this'
-console.log(likes(['Alex', 'Jacob', 'Mark', 'Max'])) // 'Alex, Jacob and 2 others like this'
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max'])) // 'Alex, Jacob and 2 others like this'g
