f24a9d33edffa018646c75c0faf90e69b5c6bd37
marukun <marukun.dev@gmail.com>
2024-10-08 23:34:08 +0300

    new codewars challenge

--- 8kyu/twich_as_old.js
+++ 8kyu/twich_as_old.js
@@ -6,5 +6,11 @@
 //  The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
 
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-    
+	return Math.abs(dadYearsOld - (sonYearsOld * 2))
 }
+
+console.log(twiceAsOld(36,7)) //  22
+console.log(twiceAsOld(55,30)) // 5
+console.log(twiceAsOld(42,21)) // 0
+console.log(twiceAsOld(22,1)) // 20
+console.log(twiceAsOld(29,0)) // 29
