3c449aeb00a5c1b1018a321f282e6f1c1e9f81c9
marukun <marukun.dev@gmail.com>
2024-10-16 18:28:00 +0300

    new codewars challenge

---
+++ 8kyu/reverse_list_order.js
@@ -0,0 +1,8 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+function reverseList(list) {
+	return list.reverse()
+}
+
+console.log(reverseList([1,2,3,4])) // [4,3,2,1]
+console.log(reverseList([3,1,5,4])) // [4,5,1,3]
