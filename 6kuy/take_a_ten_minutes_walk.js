20ea70262ea046b6eb8492a4a0f5f7af9991ef43
marukun <marukun.dev@gmail.com>
2024-10-24 05:22:18 +0300

    new codewars challenge

---
+++ 6kuy/take_a_ten_minutes_walk.js
@@ -0,0 +1,25 @@
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+
+// You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk.
+
+// The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). 
+
+// You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. 
+// Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only).
+// It will never give you an empty array (that's not a walk, that's standing still!).
+
+function isValidWalk(walk) {
+	const n = walk.filter(e => e === 'n')
+    const s = walk.filter(e => e === 's')
+    const e = walk.filter(e => e === 'e')
+    const w = walk.filter(e => e === 'w')
+    
+    return walk.length === 10 && s.length === n.length && e.length === w.length
+}
+
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s'])) //  'true'
+console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])) // 'false'
+console.log(isValidWalk(['w'])) // 'false'
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s'])) // 'false'
