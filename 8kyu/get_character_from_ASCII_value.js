4732be0dab9afa0e818e7d7598061e1d77a13c2f
marukun <marukun.dev@gmail.com>
2024-10-26 16:34:16 +0300

    new codewars challenge

---
+++ 8kyu/get_character_from_ASCII_value.js
@@ -0,0 +1,17 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+function getChar(c){
+    return String.fromCharCode(c)
+}
+
+console.log(getChar(55)) // '7'
+console.log(getChar(56)) // '8'
+console.log(getChar(57)) // '9'
+console.log(getChar(58)) // ':'
+console.log(getChar(59)) // ';'
+console.log(getChar(60)) // '<'
+console.log(getChar(61)) // '='
+console.log(getChar(62)) // '>'
+console.log(getChar(63)) // '?'
+console.log(getChar(64)) // '@'
+console.log(getChar(65)) // 'A'
