dd74ef7d4696fc73d0fd516f8f9ac642c40495e6
marukun <marukun.dev@gmail.com>
2024-10-29 15:41:19 +0200

    add new solution

--- 8kyu/merge_two_sorted_arrays_into_one.js
+++ 8kyu/merge_two_sorted_arrays_into_one.js
@@ -9,8 +9,14 @@
 // Also arr1 and arr2 may have same integers. 
 // Remove duplicated in the returned result.
 
+// Solution 1
 function mergeArrays(arr1, arr2) {
   return arr1.concat(arr2).sort((a,b) => a - b).filter((e,i,a) => a.indexOf(e) === i)
+}
+
+// Solution 2
+function mergeArrays(arr1, arr2) {
+  return [...new Set(arr1.concat(arr2))].sort((a,b)=> a - b)
 }
 
 console.log(mergeArrays([1,2,3,4], [5,6,7,8])) // [1,2,3,4,5,6,7,8]
