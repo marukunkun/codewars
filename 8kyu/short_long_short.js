4f7d1df4cbda10a9ef07b65db16baa7c0a4c6c34
marukun <marukun.dev@gmail.com>
2024-11-07 01:24:19 +0200

    codewars challenge

--- 8kyu/short_long_short.js
+++ 8kyu/short_long_short.js
@@ -1,10 +1,16 @@
 // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. 
 // The strings will not be the same length, but they may be empty ( zero length ).
 
+// Solution 1
 function solution(a, b){
 	const shot = [a, b].sort((a, b) => a.length - b.length)[0]
   const long = [a, b].sort((a, b) => a.length - b.length)[1]
   return shot + long + shot
+}
+
+// Solution 2
+function solution(a, b) {
+  return a.length < b.length ? a + b + a : b + a + b
 }
 
 console.log(solution('45', '1')) // '1451'
