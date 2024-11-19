f7190e01c1c05fe678a6ac421e53d7be3fd9d09f
marukun <marukun.dev@gmail.com>
2024-11-19 10:34:15 +0200

    second solution

--- 7kuy/greet_me.js
+++ 7kuy/greet_me.js
@@ -1,7 +1,17 @@
 // Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
 
-const greet = function(name) {
+// Solution 1
+function greet(name) {
 	return 'Hello ' + name[0].toUpperCase() + name.toLowerCase().slice(1) + '!'
+}
+
+// Solution 2
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase()
+}
+
+let greet = function(name) {
+	return "Hello " + name.capitalize() + "!"
 }
 
 console.log(greet('riley')) // 'Hello Riley!'
