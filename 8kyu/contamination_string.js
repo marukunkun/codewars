364484f7d70ff85ed16270c2f7b4ad4b6b76f4f7
marukun <marukun.dev@gmail.com>
2024-11-26 16:10:43 +0200

    codewars challenge

---
+++ 8kyu/contamination_string.js
@@ -0,0 +1,20 @@
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// Starting with the original text, and given a character, return the text once it has been mutated so that all of the characters in the original text have been replaced with the character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+function contamination(text, char) {
+	return char.repeat(text.length)
+}
+
+console.log(contamination("abc","z")) // "zzz"
+console.log(contamination("","z")) // ""
+console.log(contamination("abc","")) // ""
+console.log(contamination("_3ebzgh4","&")) // "&&&&&&&&"
+console.log(contamination("//case"," ")) // "      "
