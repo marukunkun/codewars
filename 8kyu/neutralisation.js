0fb17f431c6392bc5235bca7bed0a72130c669f5
marukun <marukun.dev@gmail.com>
2024-11-28 10:05:03 +0200

    codewars challenge

---
+++ 8kyu/neutralisation.js
@@ -0,0 +1,49 @@
+// Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+// When positives and positives interact, they remain positive.
+// When negatives and negatives interact, they remain negative.
+// But when negatives and positives interact, they become neutral, and are shown as the number 0.
+
+// Worked Example
+// ("+-+", "+--") ➞ "+-0"
+// # Compare the first characters of each string, then the next in turn.
+// # "+" against a "+" returns another "+".
+// # "-" against a "-" returns another "-".
+// # "+" against a "-" returns "0".
+// # Return the string of characters.
+
+// Notes
+// The two strings will be the same length.
+
+function neutralise(s1, s2) {
+	let result = []
+    for(let i = 0; i < s1.length; i++) {
+  	    if(s1[i] === '+' && s2[i] === '-' || s1[i] === '-' && s2[i] === '+') {
+    	    result.push('0')
+        } else {
+    	    result.push(s1[i])
+        }
+    }
+  return result.join('')
+}
+
+console.log(neutralise("--++--", "++--++")) // "000000"
+console.log(neutralise("-+-+-+", "-+-+-+")) // "-+-+-+"
+console.log(neutralise("-++-", "-+-+")) // "-+00"
+console.log(neutralise("--++", "++++")) // "00++"
+console.log(neutralise("+++--+---", "++----++-")) // "++0--000-"
+console.log(neutralise("-----", "-----")) // "-----"
+console.log(neutralise("-+", "++")) // "0+"
+console.log(neutralise("--", "-+")) // "-0"
+console.log(neutralise("-++", "+--")) // "000"
+console.log(neutralise("++-++--++-", "-+++-++-++")) // "0+0+0000+0"
+console.log(neutralise("-++-+-++-", "+-++++---")) // "00+0+000-"
+console.log(neutralise("---++-+--", "-+++--++-")) // "-00+0-+0-"
+console.log(neutralise("+-----+++-", "--+-+-++--")) // "0-0-0-++0-"
+console.log(neutralise("+-----+-", "---++-++")) // "0--00-+0"
+console.log(neutralise("-+--+-+---", "-+--+-+-+-")) // "-+--+-+-0-"
+console.log(neutralise("+-+", "-++")) // "00+"
+console.log(neutralise("-++", "-+-")) // "-+0"
+console.log(neutralise("---+", "-+++")) // "-00+"
+console.log(neutralise("+--", "+--")) // "+--"
+console.log(neutralise("--+++-+-", "+++++---")) // "00+++-0-"
