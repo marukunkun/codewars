2ff61565b4bb0e28813092d206b8f8aac7fd51ea
marukun <marukun.dev@gmail.com>
2024-12-02 16:07:00 +0200

    codewars challenge

---
+++ 8kyu/merging_sorted_integer_arrays_without_duplicates.js
@@ -0,0 +1,10 @@
+// Write a function that merges two sorted arrays into a single one. 
+// The arrays only contain integers.
+// Also, the final outcome must be sorted and not have any duplicate.
+
+function mergeArrays(arr1, arr2) {
+	return arr1.concat(arr2).filter((e,i,arr) => arr.indexOf(e) === i).sort((a,b) => a - b)
+}
+
+console.log(mergeArrays([1, 3, 5], [2, 4, 6])) //  [1, 2, 3, 4, 5, 6]
+console.log(mergeArrays([2, 4, 8], [2, 4, 6])) //  [2, 4, 6, 8]
