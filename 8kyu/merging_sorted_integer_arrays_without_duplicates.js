48ba6ac6b3c8f8140454f610c266c6a7b23caa02
marukun <marukun.dev@gmail.com>
2024-12-02 16:08:13 +0200

    second solution

--- 8kyu/merging_sorted_integer_arrays_without_duplicates.js
+++ 8kyu/merging_sorted_integer_arrays_without_duplicates.js
@@ -2,8 +2,14 @@
 // The arrays only contain integers.
 // Also, the final outcome must be sorted and not have any duplicate.
 
+// Solution 1
 function mergeArrays(arr1, arr2) {
 	return arr1.concat(arr2).filter((e,i,arr) => arr.indexOf(e) === i).sort((a,b) => a - b)
+}
+
+// Solution 2
+function mergeArrays(a, b) {
+	return [...new Set(a.concat(b))].sort((a,b) => a - b)
 }
 
 console.log(mergeArrays([1, 3, 5], [2, 4, 6])) //  [1, 2, 3, 4, 5, 6]
