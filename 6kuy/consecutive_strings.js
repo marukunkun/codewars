375f09a9045fa9e7976843daf51e28af13667738
marukun <marukun.dev@gmail.com>
2024-12-07 20:43:11 +0200

    correction solution

--- 6kuy/consecutive_strings.js
+++ 6kuy/consecutive_strings.js
@@ -23,26 +23,24 @@
 // Note
 // consecutive strings : follow one after another without an interruption
 
-function longestConsec(strarr, k) {
-	
-    if(k < 0 || strarr.length < k) {
+function longestConsec(strarr, k) {	
+    if(k <= 0 || strarr.length < k) {
         return ""
     } else {
-        let container = []
-        for(let i = 0; i < strarr.length; i++) {
-            container.push(strarr.slice(i, k+i).join(''))
-           }
-          return container.sort((a,b) => a.length - b.length).reverse()[0]
+        return strarr
+        .map((value, index) => (
+          strarr.slice(index, index+k).join('')
+        ))
+        .reduce((longest, current) => current.length > longest.length ? current : longest)
     }
-  }
-  
-  console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)) // "abigailtheta"
-  
-  console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1)) // "oocccffuucccjjjkkkjyyyeehh"
-  console.log(longestConsec([], 3)) // ""
-  console.log(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2)) // "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck"
-  console.log(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2)) // "wlwsasphmxxowiaxujylentrklctozmymu"
-  console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2)) // ""
-  console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3)) // "ixoyx3452zzzzzzzzzzzz"
-  console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15)) // ""
-  console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0)) // "" 
+}
+
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)) // "abigailtheta"
+console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1)) // "oocccffuucccjjjkkkjyyyeehh"
+console.log(longestConsec([], 3)) // ""
+console.log(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2)) // "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck"
+console.log(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2)) // "wlwsasphmxxowiaxujylentrklctozmymu"
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2)) // ""
+console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3)) // "ixoyx3452zzzzzzzzzzzz"
+console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15)) // ""
+console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0)) // "" 
