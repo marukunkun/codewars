8b9457133ab6dba083498929cbb1a271fab3b659
marukun <marukun.dev@gmail.com>
2024-12-26 23:16:20 +0200

    codewars challenge

---
+++ 8kyu/combine_strings.js
@@ -0,0 +1,10 @@
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name).
+// The function should return the full name.
+
+// Example:
+// With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+
+function combineNames(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
