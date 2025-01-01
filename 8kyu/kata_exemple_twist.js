163c7827c68cd2ebf82eddca0e8b8347fb21fcec
marukun <marukun.dev@gmail.com>
2025-01-02 01:44:27 +0200

    codewars challenge

---
+++ 8kyu/kata_exemple_twist.js
@@ -0,0 +1,11 @@
+// This is an easy twist to the example kata.
+
+// Add the value "codewars" to the array websites 1,000 times.
+
+let websites = []
+
+for(let i = 0; i < 1000; i++) {
+	websites.push('codewars')
+}
+
+console.log(websites.length)
