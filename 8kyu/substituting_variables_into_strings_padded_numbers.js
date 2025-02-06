644ce87e76425ee10c6a433a7d858d86db30d374
marukun <marukun.dev@gmail.com>
2025-02-06 18:08:54 +0200

    codewars challenge

---
+++ 8kyu/substituting_variables_into_strings_padded_numbers.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it returns a formatted string.
+// The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+function solution(value) {
+	return `Value is ${String(value).padStart(5, 0)}`
+}
+
+console.log(solution(5)) // "Value is 00005"
+console.log(solution(1204)) // "Value is 01204"
+console.log(solution(109)) // "Value is 00109"
+console.log(solution(0)) // "Value is 00000"
